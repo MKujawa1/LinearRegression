65853bb3773389d42df863afc6422cbb086e502e
MKujawa1 <maciej.kujawa96@o2.pl>
2022-06-04 18:22:06 +0200

    Added calucating r_squered

--- main.py
+++ main.py
@@ -26,11 +26,46 @@ def linear_curve(x,m,b):
     return m*x+b
 
 def generate_linear_data(x):
+    '''
+    Generate random linear curve with noise.
+
+    Parameters
+    ----------
+    x : 1d array
+        x axis for linear function.
+
+    Returns
+    -------
+    TYPE
+        array.
+
+    '''
     m = random.uniform(-0.5, 0.5)
     b = random.uniform(-0.5, 0.5)
     noise_amp = random.uniform(0.5, 2)
     noise = np.random.randn(len(x))*noise_amp
     return (m*x+b)+noise-(noise_amp/2)
+
+def r_squared(data, fit_data):
+    '''
+    Calculating r_squared value.
+
+    Parameters
+    ----------
+    data : array
+        raw data.
+    fit_data : array
+        fitted data to raw data.
+
+    Returns
+    -------
+    TYPE
+        float.
+
+    '''
+    correlation_array = np.corrcoef(data, fit_poly)
+    correlation = correlation_array[1,0]
+    return correlation**2
 
 x = np.arange(100)
 data = generate_linear_data(x)
@@ -49,20 +84,25 @@ lr.fit(x.reshape(-1,1),data.reshape(-1,1))
 m_lr = lr.coef_[0][0]
 b_lr = lr.intercept_[0]
   
+### Ploting results
 plt.figure(figsize = (14,5))
 plt.subplot(1,3,1)
 plt.title('Numpy')
 plt.scatter(x,data)
-plt.plot(fit_poly,'r',label = str(np.round(params_poly[0],3))+'*x + '+str(np.round(params_poly[1],3)))
+plt.plot(x,fit_poly,'r',label = 'm: '+str(np.round(params_poly[0],3))+'\nb: '+str(np.round(params_poly[1],3)))
 plt.legend()
 plt.subplot(1,3,2)
 plt.title('Curve_fit')
 plt.scatter(x,data)
-plt.plot(fit_cf,'r',label = str(np.round(params_cf[0],3))+'*x + '+str(np.round(params_cf[1],3)))
+plt.plot(x,fit_cf,'r',label = 'm: '+str(np.round(params_cf[0],3))+'\nb: '+str(np.round(params_cf[1],3)))
 plt.legend()
 plt.subplot(1,3,3)
 plt.title('LinearRegression')
 plt.scatter(x,data)
-plt.plot(m_lr*x+b_lr,'r',label = str(np.round(m_lr,3))+'*x + '+str(np.round(b_lr,3)))
+plt.plot(x,m_lr*x+b_lr,'r',label ='m: '+str(np.round(m_lr,3))+'\nb: '+str(np.round(b_lr,3)))
 plt.legend()
 
+### Calculate r_squared
+print('Numpy: ', r_squared(data, fit_poly))
+print('Curve_fit: ', r_squared(data, fit_cf))
+print('LinearRegression: ',r_squared(data, m_lr*x+b_lr))
