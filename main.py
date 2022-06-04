1f2f69fadf10049677e82478ae5a5ce4aef77853
MKujawa1 <maciej.kujawa96@o2.pl>
2022-06-04 17:41:16 +0200

    Linear Regression
    Added linear regression from sklearn

--- main.py
+++ main.py
@@ -5,6 +5,24 @@ import random
 from sklearn.linear_model import LinearRegression
 
 def linear_curve(x,m,b):
+    '''
+    Retunrs linear curve. Using for curve_fit 
+
+    Parameters
+    ----------
+    x : 1d array
+        x axis for data.
+    m : float
+        m parameter for function.
+    b : float
+        b parameter for function.
+
+    Returns
+    -------
+    TYPE
+        array.
+
+    '''
     return m*x+b
 
 def generate_linear_data(x):
@@ -24,21 +42,27 @@ fit_poly = np.polyval(params_poly,x)
 ### curve_fit from scipy
 params_cf, err = curve_fit(linear_curve, x, data)
 fit_cf = linear_curve(x, *params_cf)
-    
+
+### LinearRegression fit
+lr = LinearRegression()
+lr.fit(x.reshape(-1,1),data.reshape(-1,1))
+m_lr = lr.coef_[0][0]
+b_lr = lr.intercept_[0]
+  
 plt.figure(figsize = (14,5))
 plt.subplot(1,3,1)
 plt.title('Numpy')
 plt.scatter(x,data)
-plt.plot(fit_poly,'r',label = str(np.round(params_poly[0],3))+'*x + '+str(np.round(params_poly[0],3)))
+plt.plot(fit_poly,'r',label = str(np.round(params_poly[0],3))+'*x + '+str(np.round(params_poly[1],3)))
 plt.legend()
 plt.subplot(1,3,2)
 plt.title('Curve_fit')
 plt.scatter(x,data)
-plt.plot(fit_cf,'r',label = str(np.round(params_cf[0],3))+'*x + '+str(np.round(params_cf[0],3)))
+plt.plot(fit_cf,'r',label = str(np.round(params_cf[0],3))+'*x + '+str(np.round(params_cf[1],3)))
 plt.legend()
 plt.subplot(1,3,3)
-plt.title('Numpy')
+plt.title('LinearRegression')
 plt.scatter(x,data)
-plt.plot(fit_poly,'r',label = str(np.round(params_poly[0],3))+'*x + '+str(np.round(params_poly[0],3)))
+plt.plot(m_lr*x+b_lr,'r',label = str(np.round(m_lr,3))+'*x + '+str(np.round(b_lr,3)))
 plt.legend()
 
